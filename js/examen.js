ca1f0921a8288cabe5adf50bf9ab6985fe5a582a
Richard Torner <richardo@outlook.com>
2017-11-17 23:35:18 -0600

    Update examen.js
    optimizacion de codigo

--- js/examen.js
+++ js/examen.js
@@ -6,27 +6,16 @@ botoncito.addEventListener("click", clickBoton)
 
 function clickBoton() {
 	let x = parseInt(numero.value)
-
-	//int centavos = sc.nextInt();
-  let centavos = x
-    //    if (centavos > 0 && centavos <=150) {
+        let centavos = x
             let total = 0;
-            for(let c50=0; c50<=(centavos/50); c50++) {
-                // System.out.println("c50 : " + i50);
+            for(let c50=0; c50<=(centavos/50); c50++) {   
                 let centavos25 = centavos - (c50 * 50 );
-                for(let c25=0; c25<=(centavos25/25); c25++) {
-                    // System.out.println("c25 : " + i25);
+                for(let c25=0; c25<=(centavos25/25); c25++) {   
                     let centavos10 = centavos25 - (c25 * 25);
-                    for(let c10=0; c10<=(centavos10/10); c10++) {
-                        // System.out.println("c10 : " + i10);
+                    for(let c10=0; c10<=(centavos10/10); c10++) {    
                         let centavos5 = centavos10 - (c10 * 10);
-                        for(let c5=0; c5<=(centavos5/5); c5++) {
-                            // System.out.println("c5 : " + i5);
-                            let c1 = centavos5 - (c5 * 5);
-
-                            // System.out.println("c1 : " + centavos1);
-                            //console.log("c50 : " + i50 + " | c25 : " + i25 + " | c10 : " + i10 + " | c5 : " + i5 + " | c1: " + centavos1);
-                            //nformas++;
+                        for(let c5=0; c5<=(centavos5/5); c5++) {                        
+                            let c1 = centavos5 - (c5 * 5);                 
                             document.write(`Monedas: 50¢ cantidad: ${c50} , 25¢ cantidad: ${c25} , 10¢ cantidad: ${c10} , 5¢ cantidad: ${c5} , 1¢ cantidad: ${c1} <br />`)
                             total++
                         }
